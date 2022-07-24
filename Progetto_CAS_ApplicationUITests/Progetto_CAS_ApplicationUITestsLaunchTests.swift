2ff07471b1f327bebdd9e9f67f79448e31e8a698
Alessandro Benetton <aleben98@gmail.com>
2022-07-24 18:35:47 +0200

    Restart project

---
+++ Progetto_CAS_ApplicationUITests/Progetto_CAS_ApplicationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Progetto_CAS_ApplicationUITestsLaunchTests.swift
+//  Progetto_CAS_ApplicationUITests
+//
+//  Created by Alessandro Benetton on 24/07/22.
+//
+
+import XCTest
+
+class Progetto_CAS_ApplicationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
