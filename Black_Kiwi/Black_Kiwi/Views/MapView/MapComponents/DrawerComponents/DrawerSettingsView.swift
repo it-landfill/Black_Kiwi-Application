6c664da86025b247c288d29ff72fd8037189ae44
Alessandro Benetton <aleben98@gmail.com>
2022-10-04 11:17:48 +0200

    [CAS-16-settings-page] Privacy model generation

---
+++ Black_Kiwi/Black_Kiwi/Views/MapView/MapComponents/DrawerComponents/DrawerSettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  DrawerSettingsView.swift
+//  Black_Kiwi
+//
+//  Created by Alessandro Benetton on 02/10/22.
+//
+
+import SwiftUI
+
+struct DrawerSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DrawerSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        DrawerSettingsView()
+    }
+}
