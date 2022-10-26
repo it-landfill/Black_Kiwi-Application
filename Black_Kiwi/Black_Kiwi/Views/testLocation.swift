b003785a0f6b45d8b459d1cbc463458d4024c576
lorenzocrespan <lorenzo.crespan@gmail.com>
2022-10-27 00:26:52 +0200

    Add poisson noise function + Init refactoring

--- Black_Kiwi/Black_Kiwi/Views/testLocation.swift
+++ Black_Kiwi/Black_Kiwi/Views/testLocation.swift
@@ -37,7 +37,8 @@ struct testLocation: View {
                                 // Richiamo la funzione di elaborazione location e salvo i risultati in annotations
                                 print("Generating fake location")
                                 if let location = locationManager.getCoordinates() {
-                                    annotations = generateFakeLocations(location)
+                                    // TODO: Parametrizzare il numero di dummies richiesti
+                                    annotations = generateFakeLocations(location, .poisson, 10)
                                 }
                                 
                             }, label: {
@@ -97,43 +98,92 @@ struct Location: Identifiable {
     let coordinate: CLLocationCoordinate2D
 }
 
-func randomPointInDisk(radius: Double) -> (Double, Double) {
-    let angle = 2 * Double.pi * Double.random(in: 0...1)
-    let r = radius * sqrt(Double.random(in: 0...1))
-    return (r * cos(angle), r * sin(angle))
+enum NoiseDistribution {
+    case uniform
+    case gaussian
+    case poisson
+    case triangular
 }
 
-func randomLocationInRadius(userLocation: CLLocationCoordinate2D, radius: Double) -> Location {
-    let randomPoint = randomPointInDisk(radius: radius)
-    let latitude = userLocation.latitude + kilometersToDegreesLatitude(kilometers: randomPoint.0)
-    let longitude = userLocation.longitude + kilometersToDegreesLongitude(km: randomPoint.1, latitude: latitude)
-    return Location(coordinate: CLLocationCoordinate2D(latitude: latitude, longitude: longitude))
-}
-
+// Funzione che permette di trasformare da kilometri a gradi (da applicare alla latitudine)
 func kilometersToDegreesLatitude(kilometers: Double) -> Double {
     return kilometers / 111.111
 }
 
+// Funzione che permette di trasformare da kilometri a gradi (da applicare alla longitudine)
 func kilometersToDegreesLongitude(km: Double, latitude: Double) -> Double {
     return km / (111.111 * cos(latitude * Double.pi / 180))
 }
 
-func generateFakeLocations(_ location: CLLocationCoordinate2D) -> [Location] {
+func poisson_random_generator(_ lambda: Double) -> Double {
+    let L = exp(-lambda)
+    var p = 1.0
+    var k = 0
+    repeat {
+        k += 1
+        p *= Double.random(in: 0...1)
+    } while p > L
+    return Double(k - 1)
+}
+
+// Funzione che calcolato l'angolo e la distanza dal centro, ritorna le due componenti, rispettivamente
+// per la latitudine e la longitudine (distribuzione poisson).
+func poissonPointInDisk(radius: Double, lam: Double, k: Int) -> (Double, Double) {
+    let r = radius * sqrt(poisson_random_generator(lam))
+    print("Poisson raggio: ", r)
+    let angle = 2 * Double.pi * Double.random(in: 0...1)
+    return (r * cos(angle), r * sin(angle))
+}
+
+// Funzione che calcolato l'angolo e la distanza dal centro, ritorna le due componenti, rispettivamente
+// per la latitudine e la longitudine (distribuzione uniforme).
+func uniformPointInDisk(radius: Double) -> (Double, Double) {
+    let r = radius * sqrt(Double.random(in: 0...1))
+    let angle = 2 * Double.pi * Double.random(in: 0...1)
+    return (r * cos(angle), r * sin(angle))
+}
+
+// Funzione che ritorna una "Location" applicando un determinato rumore alla "userLocation".
+func randomLocationInRadius(userLocation: CLLocationCoordinate2D,_ distribution: NoiseDistribution, radius: Double) -> Location {
+    var deltaLocation : (Double, Double) = (0, 0)
+    switch distribution {
+    case .uniform:
+        print("Uniform distribution")
+        deltaLocation = uniformPointInDisk(radius: radius)
+    case .poisson:
+        print("Poisson distribution")
+        deltaLocation = poissonPointInDisk(radius: radius, lam: 20, k: 1)
+    case .triangular:
+        print("Trinagular distribution")
+        // deltaLocation = randomPointInDisk(radius: radius)
+    case .gaussian:
+        print("Gauss distribution")
+        // deltaLocation = randomPointInDisk(radius: radius)
+    }
+    let latitude = userLocation.latitude + kilometersToDegreesLatitude(kilometers: deltaLocation.0)
+    let longitude = userLocation.longitude + kilometersToDegreesLongitude(km: deltaLocation.1, latitude: latitude)
+    return Location(coordinate: CLLocationCoordinate2D(latitude: latitude, longitude: longitude))
+}
+
+func generateFakeLocations(_ location: CLLocationCoordinate2D,_ distribution: NoiseDistribution,_ dummiesRequested: Int) -> [Location] {
     // Number of dummies.
-    let count = 10
+    let maxDummies = 20
     // List of random locations.
     var locations: [Location] = []
     
-    for _ in 0..<count {
-        locations.append(randomLocationInRadius(userLocation: location, radius: 0.5))
-    }
+    //    Errore: dummiesRequested viene gestita come una variabile let e non possibile assegnarci valori
+    //    if dummiesRequested > maxDummies {
+    //        dummiesRequested = maxDummies
+    //    }
     
-    for location in locations{
-        print(location)
+    for _ in 0..<dummiesRequested {
+        locations.append(randomLocationInRadius(userLocation: location, distribution, radius: 0.5))
     }
     
     return locations
 }
+
+
 
 struct testAPI_Previews: PreviewProvider {
     static var previews: some View {
